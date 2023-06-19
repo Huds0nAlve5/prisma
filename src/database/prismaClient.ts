a73252db78060df59675dc3af3794fa1520fd698
SUPF5009 <hudsonalvesfl@gmail.com>
2023-06-19 12:41:03 -0300

    adicionado função de conexão com o banco de dados

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -2,4 +2,29 @@ import { PrismaClient } from "@prisma/client";
 
 const prismaClient = new PrismaClient()
 
-export { prismaClient };
+async function main() {
+    const conta = await prismaClient.receita.create(
+        {
+            data: {
+                valor: 100,
+                conta: {
+                    connect: {
+                        codigo: 1
+                    }
+                }
+            }
+        }
+    )
+
+    console.log(conta)
+
+    const deleteConta = await prismaClient.receita.delete(
+        {
+            where: {
+                codigo: 2
+            }
+        }
+    )
+  }
+  
+main()
