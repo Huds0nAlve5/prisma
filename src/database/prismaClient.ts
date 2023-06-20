49d84fddd462dc2b4aa5df7ba7d4981495714c12
Hudson Alves <hudsonalvesfl@gmail.com>
2023-06-19 22:21:41 -0300

    finalizado a documentação basica do prisma

--- src/database/prismaClient.ts
+++ src/database/prismaClient.ts
@@ -3,50 +3,67 @@ import { PrismaClient } from "@prisma/client";
 const prismaClient = new PrismaClient()
 
 async function main() {
-    const criar_conta = await prismaClient.conta.create( //create
-        {
-            data: {
-                conta: "nubank",
-                saldo: 100
-            }
-        }
-    )
+    // const criar_conta = await prismaClient.conta.create( //create
+    //     {
+    //         data: {
+    //             conta: "nubank",
+    //             saldo: 100
+    //         }
+    //     }
+    // )
 
-    const consultar_conta = await prismaClient.conta.findMany( //select
-        {
+    // const consultar_conta = await prismaClient.conta.findMany( //select
+    //     {
             
-        }
-    )
+    //     }
+    // )
 
-    const atualizar_conta = await prismaClient.conta.update(  //update
-        {
-            where: {
-                codigo: 1
-            },
-            data: {
-                conta: "inter"
-            }
-        }
-    )
+    // const atualizar_conta = await prismaClient.conta.update(  //update
+    //     {
+    //         where: {
+    //             codigo: 1
+    //         },
+    //         data: {
+    //             conta: "inter"
+    //         }
+    //     }
+    // )
 
-    const new_receita = await prismaClient.receita.create(
-        {
-            data: {
-                valor: 200,
-                conta: {
-                    connect: {
-                        codigo: 1
-                    }
+    // const new_receita = await prismaClient.receita.create(
+    //     {
+    //         data: {
+    //             valor: 200,
+    //             conta: {
+    //                 connect: {
+    //                     codigo: 1
+    //                 }
+    //             }
+    //         }
+    //     }
+    // )
+
+    // const receitaComConta = await prismaClient.receita.findMany({
+    //     include: {
+    //         conta: true
+    //     }
+    // })
+
+    const selectCampos = await prismaClient.receita.findFirst({
+        where: {
+            codigo: 21
+        },
+        select: {
+            conta: {
+                select: {
+                    conta: true
                 }
-            }
+            },
+            valor: true
         }
-    )
-
-    const receitaComConta = await prismaClient.receita.findMany({
-        include: {
-            conta: true
-        }
-    })
+    })  //select são os campos que eu quero, e o .conta() no final faz o objeto conta ser associado a variavel
+    //o include inclui o objeto referenciado na tabela como FK, assim traz os dados da outra tabela
+    //pelo visto, o select: {conta: true} faz o mesmo que o include: {conta: true}, com a diferença que o
+    //include traz os outros dados da tabela, o select nao
 
     //const deleteReceita = await prismaClient.receita.delete(
     //    {
@@ -62,21 +79,43 @@ async function main() {
         }
     })
 
-    const deleteReceitasComFiltro = await prismaClient.receita.deleteMany(          //deletar baseado no codigo da conta
+    const saldoContaNu = await prismaClient.conta.aggregate( // faz calculos
         {
+            _sum: {
+                saldo: true
+            },
             where: {
-                conta_id: conta?.codigo
+                conta: "nubank"
             }
         }
     )
 
-    console.log(criar_conta)
-    console.log(consultar_conta)
-    console.log(atualizar_conta)
-    console.log(new_receita)
-    console.log(receitaComConta)
-    console.log(conta?.codigo)
-    console.log(deleteReceitasComFiltro)
+    const saldoPorConta = await prismaClient.conta.groupBy({
+        by: ['conta'],
+        _sum: {
+            saldo: true
+        }
+    })
+
+    console.log(saldoContaNu)
+    console.log(saldoPorConta)
+
+    // const deleteReceitasComFiltro = await prismaClient.receita.deleteMany(          //deletar baseado no codigo da conta
+    //    {
+    //        where: {
+    //            conta_id: conta?.codigo
+    //        }
+    //    }
+    // )
+
+    //console.log(criar_conta)
+    //console.log(consultar_conta)
+    // console.log(atualizar_conta)
+    // console.log(new_receita)
+    // console.log(receitaComConta)
+    // console.log(conta?.codigo)
+    //console.log(deleteReceitasComFiltro)
+    console.log(selectCampos)//
   }
   
 main()
